68794091ee9b5db60ce10a654e91ebd1762606b1
KristinaDudnyk <113212730+KristinaDudnyk@users.noreply.github.com>
2022-09-29 23:52:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-var pages;
+var comment;
 
-
-pages = ['A farmer had a dog who used to wait by the roadside for vehicles to come.', 'As soon as one came, he would run down the road, barking and trying to overtake the car.', 'One day the farmer’s neighbor asked the farmer, “Do you think the dog is ever going to overtake those vehicles?”', 'The farmer replied, “That is not what bothers me. What bothers me is what he would do if he ever caught one.”', 'Many people in life behave like that dog who is pursuing meaningless goals.'];
-
-
-document.getElementById('turn_page').addEventListener('click', (event) => {
-  let element_list = document.getElementById('list');
-  if (!!pages.length) {
-    let new_li = document.createElement('li');
-    new_li.innerText = pages.shift();
-
-    element_list.appendChild(new_li);
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
   }
-
-});
-
-document.getElementById('the_end').addEventListener('click', (event) => {
-  let element_end = document.getElementById('end');
-  let new_img = document.createElement('img');
-  new_img.setAttribute("src", 'https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg');
-
-  element_end.appendChild(new_img);
-
-});
-
-document.getElementById('repeat').addEventListener('click', (event) => {
-  let element_list2 = document.getElementById('list');
-  element_list2.replaceChildren();
-
-});
-if (!!pages.length) {
 }
+
+
+let element_comment = document.getElementById('comment');
+element_comment.replaceChildren();
+comment = [];
+
+
+document.getElementById('button-comment').addEventListener('click', (event) => {
+  comment.unshift(getNumberOrString(document.getElementById('text-area').value));
+  let element_comment2 = document.getElementById('comment');
+  let new_li = document.createElement('li');
+  new_li.innerText = comment.shift();
+
+  element_comment2.appendChild(new_li);
+
+});
